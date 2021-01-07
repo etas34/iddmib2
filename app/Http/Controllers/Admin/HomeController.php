531201668140ac06b81e41df5c25d32254b47173
PC <orhanerday@gmail.com>
2021-01-07 18:27:05 +0300

    db

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Duyuru;
+use App\Models\Etkinlik;
+use App\Models\Haber;
+use App\Models\Sektor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 
@@ -10,7 +14,12 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $haber = Haber::all();
+        $sektor = Sektor::where('durum',1);
+        $duyuru = Duyuru::all();
+        $etkinlik = Etkinlik::all();
+        $compact = compact('etkinlik','duyuru','haber','sektor');
+        return view('admin.dashboard',$compact);
     }
 
     public function form_index()
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,16 +1,87 @@
 <x-admin-app>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
-        </div>
+
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Gösterge Paneli</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Gösterge Paneli</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{$haber->count()}}</h3>
 
+                            <p>Haberler</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-newspaper"></i>
+                        </div>
+                        <a href="{{route('admin.duyuru.index')}}" class="small-box-footer">Git <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{$sektor->count()}}</h3>
 
+                            <p>Sektörler</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-map-signs"></i>
+                        </div>
+                        <a href="{{route('admin.sektor.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>{{$duyuru->count()}}</h3>
 
+                            <p>Duyurular</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-check"></i>
+
+                        </div>
+                        <a href="{{route('admin.duyuru.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>{{$etkinlik->count()}}</h3>
+
+                            <p>Etkinlikler</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-clipboard-check"></i>
+                        </div>
+                        <a href="{{route('admin.etkinlik.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+            </div>
+            </div>
 @push('scripts')
 
 
